50f439ac0580da01320e5234637a33f8309a3a92
Anis mahmud Ratul <anismahmudratul@Aniss-MacBook-Air.local>
2024-08-12 23:59:31 +0600

    added calculate area of triangle

---
+++ javaScript-basic-part-A/triangle.js
@@ -0,0 +1,4 @@
+const base = Number(prompt("Enter a triangle base number:"));
+const height = Number(prompt("Enter a triangle height number:"));
+const triangle = 0.5 + base + height;
+console.log("Area of triangle = " + triangle);
