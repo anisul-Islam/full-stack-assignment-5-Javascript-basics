dd1287fc1fa247f903722fc1182d402a9086dcbb
Mohammed Ashraful Islam Shafin <mai5hafin2002@gmail.com>
2024-08-13 01:59:45 +0600

    Completed the assignment 5

---
+++ circle.js
@@ -0,0 +1,7 @@
+function circle(){
+    let radius= Number(prompt("Enter Circle radius : "));
+
+    let area=3.1416*radius*radius;
+    alert(`Circle area= ${area}`);
+}
+
---
+++ triangle.js
@@ -0,0 +1,8 @@
+function triangle(){
+
+    let base= Number(prompt("Enter triangle base : "));
+    let height= Number(prompt("Enter triangle height : "));
+    let area=base*height*0.5;
+    alert(`Triangle area= ${area}`);
+}
+
