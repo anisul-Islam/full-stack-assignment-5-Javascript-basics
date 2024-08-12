10940ab0f2f87f859663d3fa00ee7015fb2444e4
Anis mahmud Ratul <anismahmudratul@Aniss-MacBook-Air.local>
2024-08-13 00:11:52 +0600

    added calculate area of rectangle

---
+++ javaScript-basic-part-A/rectangle.js
@@ -0,0 +1,4 @@
+const length = Number(prompt("Enter a rectangle length number:"));
+const width = Number(prompt("Enter a rectangle width number:"));
+const rectangle = length * width;
+console.log("Area of rectangle = " + rectangle);
