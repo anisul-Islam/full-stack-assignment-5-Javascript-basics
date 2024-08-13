561aea89ae786f3689a5ef73da1127ef0eb47c76
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-08-13 10:12:31 +0600

    succcessfully completed assainment-5

---
+++ Basic-Javascript-assainment/rectangle.js
@@ -0,0 +1,7 @@
+// (area of rectangle = length*width)
+
+const width = Number(prompt("please Enter rectangle width : "));
+const length = Number(prompt("please Enter rectangle length : "));
+
+const rectangleArea = length * width ;
+console.log(`rectangle area = ${rectangleArea}`);
---
+++ Basic-Javascript-assainment/triangle.js
@@ -0,0 +1,12 @@
+
+// (area of triangle = 0.5 * base * height)
+
+const base = Number(prompt("Please enter triangle base : "));
+const height = Number(prompt("Please enter triangle height : "));
+
+const triangleArea = 0.5 * (base * height);
+
+console.log(`triangle area: ${triangleArea}`);
+
+
+// (area of rectangle = length*width)
