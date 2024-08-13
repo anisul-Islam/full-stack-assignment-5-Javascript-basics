d6ccd334b97d3b88a0888bbed6f327266cb9aff3
mohammad-nayem <nishanahmed13913@gmail.com>
2024-08-14 01:09:01 +0600

    Js first assignmnet

---
+++ JS file/circle.js
@@ -0,0 +1,6 @@
+const radius = Number(prompt("Enter the Circle's radius number below"));
+ 
+
+const circle = 3.1416 * radius * radius;
+
+document.getElementById('circle').innerHTML = `Triangle area is: ${circle}`
