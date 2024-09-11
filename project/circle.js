ff5bd033d0a9cafbed9af69452e51fd17b20a3b7
sourov-chandra <sourovtpi@yahoo.com>
2024-09-11 21:53:39 +0600

    finished the assignment number-5

---
+++ project/circle.js
@@ -0,0 +1,5 @@
+function calculateCircle() {
+  let radius = parseFloat(prompt("Enter the radius of the circle:"));
+  let areaOfCircle = 3.1416 * radius * radius;
+  alert("The area of the circle is: " + areaOfCircle);
+}
---
+++ project/triangle.js
@@ -0,0 +1,6 @@
+function calculateTriangle() {
+  let base = parseFloat(prompt("Enter the base of the triangle:"));
+  let height = parseFloat(prompt("Enter the height of the triangle:"));
+  let areaOfTriangle = 0.5 * base * height;
+  alert("The area of the triangle is: " + areaOfTriangle);
+}
