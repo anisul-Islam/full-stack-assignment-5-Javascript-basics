c406fc6ecdae45f1aac4ef0ef41829248e22a7fd
bukharibd <bukharibd@gmial.com>
2024-11-27 16:48:59 +0600

    created 3 calculater for measuring the area of a traingle, rectangle and circle

---
+++ basic-calculator/triangle.js
@@ -0,0 +1,4 @@
+const base = Number(prompt("Enter the base of a traingle: "));
+const height = Number(prompt("Enter the height of a traingle: "));
+
+console.log(`Area of the traingle is ${0.5 * base * height}`);
